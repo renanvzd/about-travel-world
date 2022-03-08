213013b84ed2f97d8f25df3a2399b4b6d5d83dad
Renan Veronez Drechsler <renanvz@outlook.com>
2022-03-08 15:56:45 -0300

    FormHistory

--- src/components/FormHistory/index.tsx
+++ src/components/FormHistory/index.tsx
@@ -1,10 +1,37 @@
-import Form from './Form';
+import { useEffect, useState } from 'react';
+// import Form from './Form';
 import { Container } from './styles';
 
+type Repository = {
+  title: string;
+  description: string;
+}
+
+
 function FormHistory() {
+  const [repositories, setRepositories] = useState<Repository[]>([])
+
+  useEffect(() => {
+    fetch('https://ola-task.herokuapp.com/posts')
+      .then(response => response.json())
+      .then(data => {
+        setRepositories(data);
+      })
+  }, [])
+
   return (
     <Container>
-      <Form />
+      <ul>
+        {repositories.map(repo => {
+          return (
+            <li key={repo.title}>
+              <strong>{repo.title}</strong>
+              <p>{repo.description}</p>
+            </li>
+          )
+        })}
+      </ul>
+
     </Container>
   );
 }
